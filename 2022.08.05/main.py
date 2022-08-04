91a278009427d2a149a34854a22b6af84516708f
梁深南 <110004355+Draymond-Sreike@users.noreply.github.com>
2022-08-05 00:07:17 +0800

    Create main.py

---
+++ 2022.08.05/main.py
@@ -0,0 +1,12 @@
+n1 = 1
+n2 = -1
+n3 = 0
+print(n1,id(n1),type(n1))
+print(n2,id(n2),type(n2))
+print(n3,id(n3),type(n3))
+
+n4 = 1
+n5 = 3.141596
+n6 = True
+str(n4)
+print(type(str(n4)), type(n4)) #n4在调用tr函数之后并没有转换为str类型
