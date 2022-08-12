5388aeed7f12fad983a29929dffb07bc34e43a0c
梁深南 <110004355+Draymond-Sreike@users.noreply.github.com>
2022-08-12 20:25:18 +0800

    Create Day20220812.py

---
+++ Day20220812.py
@@ -0,0 +1,7 @@
+print('123四'.isnumeric())
+print('123四'.isalnum())
+print('abc四'.isalnum())
+print(ord('梁'))
+# print(ord('梁深南'))         #不可行，不能找字符串对应的编码值
+s = 'Hello,Python'[-1:-7:-1]
+print(s)                      #nohtyP
